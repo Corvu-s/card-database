6c2cdcb87b16740e4136d11531b04b28ba31733a
Corvu-s <roomtemp72@gmail.com>
2021-10-20 15:14:21 -0400

    coolness

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,19 @@
 import Navbar from "../components/Navbar";
 import CardView from "../components/CardView";
+import Filter from "../components/Filter"
+import {useState} from 'react'
+
 //https://www.section.io/engineering-education/google-drive-api-nodejs/
 export default function Home({ data }) {
+  let collectionData = [{name:"Dominaria",code:"DOM",},{name:"Guilds Of Ravnica",code:"GRN"},{name:"Ravnica Allegiance",code:"RNA"},{name:"War of the Spark",code:"WAR"},{name:"Throne of Eldraine",code:"ELD"}]
+
+  const [collectionNames,setCollection] = useState(collectionData)
+  const [collectionChoice,setCollectionChoice]=useState("");
   //console.log(data);
   return (
     <div>
       <Navbar />
+      <Filter/>
       <CardView message={data} />
     </div>
   );
